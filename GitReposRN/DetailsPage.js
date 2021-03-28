fcda1474bc5cf4f76979eca3d8df298cd8530855
Razvan Rujoiu <rjrazvan@icloud.com>
2021-03-28 23:19:29 +0300

    passed item to details page

--- GitReposRN/DetailsPage.js
+++ GitReposRN/DetailsPage.js
@@ -17,19 +17,55 @@ import {
   View,
 } from 'react-native';
 
-
 import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-  } from 'react-native/Libraries/NewAppScreen';
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-export function DetailsPage({ navigation }) {
-    return (<View style = {{ flex: 1 }}>
-        <Text>Hello world</Text>
-    </View>)
-    
+export function DetailsPage({route, navigation}) {
+  const {item} = route.params;
+
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: '#ffffa6'}}>
+      <View style={styles.topView}>
+        <Image source={{uri: item.owner.avatar_url}} style={styles.image} />
+        <Text style={styles.repoTitle}>{item.full_name}</Text>
+        <Text style={styles.repoDescription}>{item.description}</Text>
+      </View>
+      <View style={styles.bottomView}></View>
+    </SafeAreaView>
+  );
 }
 
+const styles = StyleSheet.create({
+  topView: {
+    flex: 4,
+    backgroundColor: '#dce775',
+    borderBottomLeftRadius: 44,
+    borderBottomRightRadius: 44,
+  },
+  bottomView: {
+    flex: 2,
+    backgroundColor: '#ffffa6',
+  },
+  image: {
+    marginTop: 24,
+    height: 150,
+    width: 150,
+    alignSelf: 'center',
+    borderRadius: 150,
+  },
+  repoTitle: {
+    textAlign: 'center',
+    marginTop: 12,
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  repoDescription: {
+    margin: 12,
+    textAlign: 'center',
+  },
+});
--- GitReposRN/HomePage.js
+++ GitReposRN/HomePage.js
@@ -17,111 +17,129 @@ import {
   View,
 } from 'react-native';
 
-import { useNavigation } from '@react-navigation/native'
-
+import {useNavigation} from '@react-navigation/native';
 
 import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-  } from 'react-native/Libraries/NewAppScreen';
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-import { DetailsPage } from './DetailsPage';
+import {DetailsPage} from './DetailsPage';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 export class HomePage extends Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        data: [],
-        isLoading: true
-      }
-    }
-  
-    componentDidMount() {
-      fetch('https://api.github.com/search/repositories?q=language:javascript&order=desc&sort=stars')
-        .then((response) => response.json())
-        .then((json) => {
-          this.setState({ data: json.items });
-        })
-        .catch((error) => console.error(error))
-        .finally(() => {
-          this.setState({ isLoading: false });
-        });
-    }
+  constructor(props) {
+    super(props);
 
-    componentWillUnmount() {
-      // fix Warning: Can't perform a React state update on an unmounted component
-      this.setState = (state,callback)=>{
-          return;
-      };
+    this.state = {
+      data: [],
+      isLoading: true,
+    };
   }
-  
-    customCell = (item) => {
-      return (<TouchableOpacity style = {styles.button} onPress = { () => { this.onCellPress() }} >
-        <Text style = {{ fontSize: 16, fontWeight: 'bold', textAlign: 'center' }}> {item.name} </Text>
-        <Text style = {{ marginTop: 8 }}> {item.description} </Text>
-        <Image source = {{ uri: item.owner.avatar_url }} style = {{ marginTop: 12, height : 100, width: 100, alignSelf: 'center', borderRadius: 24 }}></Image>
-      </TouchableOpacity>)
-    }
-  
-    onCellPress = () => {
-        this.props.navigation.push('Details');
-    }
-  
-    render() {
-      
-      const { data, isLoading } = this.state;
-  
-      return (
-          <View style = { [{ backgroundColor: '#ffffa6'}, styles.sectionContainer] }>
-            { isLoading ? <ActivityIndicator/> : (
-              <FlatList
-                data = { data }
-                keyExtractor = {( { id }, index) => id}
-                renderItem = { ( {item}) => this.customCell(item) }
-              />
-            )}
-          </View>
-      )
-    }
+
+  componentDidMount() {
+    fetch(
+      'https://api.github.com/search/repositories?q=language:javascript&order=desc&sort=stars',
+    )
+      .then(response => response.json())
+      .then(json => {
+        this.setState({data: json.items});
+      })
+      .catch(error => console.error(error))
+      .finally(() => {
+        this.setState({isLoading: false});
+      });
   }
-    
-  const styles = StyleSheet.create({
-    sectionContainer: {
-      flex: 1,
-      marginTop:  32,
-    },
-    sectionTitle: {
-      fontSize: 20,
-      fontWeight: '600',
-    },
-    sectionDescription: {
-      marginTop: 8,
-      fontSize: 18,
-      fontWeight: '400',
-    },
-    highlight: {
-      fontWeight: '700',
-    },
-    item : {
-      backgroundColor: '#f9c3bb',
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 12,
-    },
-    button: {
-      flex: 1,
-      flexDirection : "column",
-      margin: 8,
-      backgroundColor: '#dce775',
-      padding: 12,
-      borderRadius: 12
-    }
-  });
-  
+
+  componentWillUnmount() {
+    // fix Warning: Can't perform a React state update on an unmounted component
+    this.setState = (state, callback) => {
+      return;
+    };
+  }
+
+  customCell = item => {
+    return (
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          this.onCellPress(item);
+        }}>
+        <Text style={{fontSize: 16, fontWeight: 'bold', textAlign: 'center'}}>
+          {' '}
+          {item.name}{' '}
+        </Text>
+        <Text style={{marginTop: 8}}> {item.description} </Text>
+        <Image
+          source={{uri: item.owner.avatar_url}}
+          style={{
+            marginTop: 12,
+            height: 100,
+            width: 100,
+            alignSelf: 'center',
+            borderRadius: 24,
+          }}></Image>
+      </TouchableOpacity>
+    );
+  };
+
+  onCellPress = item => {
+    this.props.navigation.push('Details', {item: item});
+  };
+
+  render() {
+    const {data, isLoading} = this.state;
+
+    return (
+      <SafeAreaView style={styles.sectionContainer}>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+            data={data}
+            keyExtractor={({id}, index) => id}
+            renderItem={({item}) => this.customCell(item)}
+          />
+        )}
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    flex: 1,
+    backgroundColor: '#ffffa6',
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  item: {
+    backgroundColor: '#f9c3bb',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 12,
+  },
+  button: {
+    flex: 1,
+    flexDirection: 'column',
+    margin: 8,
+    backgroundColor: '#dce775',
+    padding: 12,
+    borderRadius: 12,
+  },
+});
